d4ab94302ceb235db8f89bd23615e7d5ebaf0632
Thomas Hobson <iamthomasnz@gmail.com>
2019-07-13 16:42:43 +1200

    Fixed some webpacking issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,65 +2,68 @@ var jsonImporter = require('node-sass-json-importer');
 const path = require('path');
 const fs = require('fs');
 
-
 var cfg = require('./src/config.json');
 var displayCfg = JSON.stringify(cfg.displays);
-var webpack = require("webpack");
+var webpack = require('webpack');
 
-fs.writeFile("./src/dispconf.json", displayCfg, function(err) {
-    if(err) {
-        throw new Error ("Could not save display config");
-    }
-}); 
-
-
+fs.writeFile('./src/dispconf.json', displayCfg, function(err){
+	if (err) {
+		throw new Error('Could not save display config');
+	}
+});
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        })
-    ],
-  module:{
-  rules:[
-        {
-            test:/\.(s*)css$/,
-            use: [
-                'style-loader',
-                {
-                    loader: 'css-loader',
-                    options: {
-                        importLoaders: 1
-                    },
-                },
-                {
-                    loader: 'sass-loader',
-                    // Apply the JSON importer via sass-loader's options.
-                    options: {
-                        importer: jsonImporter(),
-                    },
-                },
-            ],
-        },
-        {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [
-            {
-                loader: 'file-loader',
-                options: {
-                name: '[name].[ext]',
-                outputPath: 'fonts/'
-                }
-            }
-            ]
-      }
-    ]
-  }
+	entry: './src/index.js',
+	mode: 'production',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(s*)css$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1
+						}
+					},
+					{
+						loader: 'sass-loader',
+						// Apply the JSON importer via sass-loader's options.
+						options: {
+							importer: jsonImporter()
+						}
+					}
+				]
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+			},
+			{ test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'fonts/'
+						}
+					}
+				]
+			}
+		]
+	}
 };
